1b4a36c4ea78d2e5c3dab55afece6aca57ac9d75
Sina Shamsizadeh <sina_shamsizadeh@hotmail.com>
2023-11-03 13:14:57 +0330

    Update empty-cart.php

--- includes/cart/empty-cart.php
+++ includes/cart/empty-cart.php
@@ -3,8 +3,7 @@
 add_action( 'edd_cart_contents_loaded', 'ma_empty_cart_template' );
 // add_filter( 'the_content', 'filter_the_content_in_the_main_loop', 1 );
 function ma_empty_cart_template() {
-	if ( is_page( 631 ) ) {
-		var_dump( did_action( 'edd_checkout_cart_item_title_after' ) );
+	if ( is_page( 631 ) && ! did_action( 'edd_checkout_cart_item_title_after' ) ) {
 		echo '<img src="' . esc_url( EDD_CUSTOM_URL . 'assets/images/svg/empty-cart.svg' ) . '" alt="empty-cart" class="empty-cart">';
 	}
 }
