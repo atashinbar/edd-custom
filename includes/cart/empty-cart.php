671dbd185e057994d9ff6fd0025d8816cfdbe3f0
Sina Shamsizadeh <sina_shamsizadeh@hotmail.com>
2023-11-03 12:48:16 +0330

    Update empty-cart.php

--- includes/cart/empty-cart.php
+++ includes/cart/empty-cart.php
@@ -3,6 +3,6 @@
 add_action( 'edd_cart_contents_loaded', 'ma_empty_cart_template' );
 
 function ma_empty_cart_template() {
-	// var_dump( edd_get_cart_contents() );
+	var_dump( is_page( 631 ) );
 	echo 'test';
 }
