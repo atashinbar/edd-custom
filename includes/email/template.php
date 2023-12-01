49b4e2a7e2d8b27753d47a718102a0619b8af17f
Sina Shamsizadeh <sina_shamsizadeh@hotmail.com>
2023-12-01 23:07:23 +0330

    Update template.php

--- includes/email/template.php
+++ includes/email/template.php
@@ -8,11 +8,76 @@
  */
 defined( 'ABSPATH' ) || exit;
 
+/**
+ * Processes the creation of a new renewal notice
+ *
+ * @since 3.0
+ * @param array $data The post data
+ * @return void
+ */
+function edd_sl_process_add_renewal_notice( $data ) {
 
-// کلید مجوز شما در حال منقضی شدن است
-remove_action( 'edd_edit_renewal_notice', 'edd_sl_process_update_renewal_notice' );
-add_action( 'edd_edit_renewal_notice', 'moadian_abzar_edd_sl_process_update_renewal_notice' );
-function moadian_abzar_edd_sl_process_update_renewal_notice( $data ) {
+	if( ! is_admin() ) {
+		return;
+	}
+
+	if( ! current_user_can( 'manage_shop_settings' ) ) {
+		wp_die( __( 'You do not have permission to add renewal notices', 'edd_sl' ), __( 'Error', 'edd_sl' ), array( 'response' => 401 ) );
+	}
+
+	if( ! wp_verify_nonce( $data['edd-renewal-notice-nonce'], 'edd_renewal_nonce' ) ) {
+		wp_die( __( 'Nonce verification failed', 'edd_sl' ), __( 'Error', 'edd_sl' ), array( 'response' => 401 ) );
+	}
+
+	$subject = isset( $data['subject'] ) ? sanitize_text_field( $data['subject'] ) : __( 'Your License Key is About to Expire', 'edd_sl' );
+	$period  = isset( $data['period'] )  ? sanitize_text_field( $data['period'] )  : '+1month';
+	$message = isset( $data['message'] ) ? stripslashes( $data['message'] ) : false;
+	$result  = 'success';
+	$notice  = __( 'Renewal Notice saved successfully.', 'edd_sl' );
+
+	if ( empty( $message ) ) {
+		$result  = 'warning';
+		$notice  = __( 'Your message was empty and could not be saved. It has been reset to the default.', 'edd_sl' );
+		$message = edd_sl_get_default_renewal_notice_message();
+	}
+
+	$notices   = edd_sl_get_renewal_notices();
+	$key       = is_array( $notices ) ? count( $notices ) : 1;
+	$notices[] = array(
+		'subject'     => $subject,
+		'message'     => $message,
+		'send_period' => $period,
+	);
+
+	update_option( 'edd_sl_renewal_notices', $notices );
+
+	$redirect_url = add_query_arg(
+		array(
+			'post_type'     => 'download',
+			'page'          => 'edd-license-renewal-notice',
+			'edd_sl_action' => 'edit-renewal-notice',
+			'notice'        => urlencode( $key ),
+			'edd-message'   => urlencode( $notice ),
+			'edd-result'    => urlencode( $result ),
+		),
+		admin_url( 'edit.php' )
+	);
+
+	wp_safe_redirect( $redirect_url );
+	exit;
+
+}
+remove_action( 'edd_add_renewal_notice', 'edd_sl_process_add_renewal_notice' );
+add_action( 'edd_add_renewal_notice', 'modian_abzar_edd_sl_process_add_renewal_notice' );
+
+/**
+ * Processes the update of an existing renewal notice
+ *
+ * @since 3.0
+ * @param array $data The post data
+ * @return void
+ */
+function edd_sl_process_update_renewal_notice( $data ) {
 
 	if( ! is_admin() ) {
 		return;
@@ -68,3 +133,5 @@ function moadian_abzar_edd_sl_process_update_renewal_notice( $data ) {
 	exit;
 
 }
+remove_action( 'edd_edit_renewal_notice', 'edd_sl_process_update_renewal_notice' );
+add_action( 'edd_edit_renewal_notice', 'modian_abzar_edd_sl_process_update_renewal_notice' );
